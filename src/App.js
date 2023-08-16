2e768f284ad37978424ca49bf27f0dfcd7776e1c
Satyam <satyampandit021@gmail.com>
2023-08-17 04:16:41 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import react, { useEffect } from 'react'
+import react, { useEffect, useState } from 'react'
 import axios from 'axios'
 function App() {
+  const [text, , setText] = useState("hellow")
   useEffect(() => {
+
     const fetchData = async () => {
-      await axios.get("http://localhost:8000/table").then((res) => {
+      await axios.post("http://localhost:8000/table", { text }).then((res) => {
         console.log(res.data)
       })
     }
     fetchData()
-  }, [])
+  }, [text])
   return (
     < div className="App" >
       <div className="App-header">
         Hey Data is Coming ?
+
+        <button>Send data To Backend!</button>
       </div>
     </ div >
   );
